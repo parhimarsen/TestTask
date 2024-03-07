3ad61db67306cf7327408ea27e393cb00fcd83e1
parhimarsen <parhimarsen@mail.ru>
2024-03-07 14:07:04 +0300

    Add project files.

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
