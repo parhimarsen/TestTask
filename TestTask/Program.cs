1fd0184e105bcdcc5395bd6cda45565faa640440
parhimarsen <parhimarsen@mail.ru>
2024-05-11 17:17:52 +0300

    1 - Add dependencies

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using TestTask.Data;
+using TestTask.Services.Implementations;
+using TestTask.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
