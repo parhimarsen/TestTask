4447a46d09040d8ccdde5e6459ab7bf330a79a69
parhimarsen <parhimarsen@mail.ru>
2024-05-11 17:18:10 +0300

    2 - Add implementation services

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            var order = _context.Orders
+                .Where(o => o.Quantity > 1)
+                .OrderByDescending(o => o.CreatedAt)
+                .FirstOrDefaultAsync();
+
+            return order;
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            var activeUserIds = _context.Users
+                .Where(u => u.Status == UserStatus.Active)
+                .Select(u => u.Id);
+
+            var orders = _context.Orders
+                .Where(o => activeUserIds.Contains(o.Id))
+                .OrderBy(o => o.CreatedAt)
+                .ToListAsync();
+
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var user = await _context.Users
+                .Select(u => new
+                {
+                    User = u,
+                    CommonPrice = u.Orders.Where(o => o.CreatedAt.Year == 2003).Sum(o => o.Quantity * o.Price)
+                })
+                .OrderByDescending(x => x.CommonPrice)
+                .FirstOrDefaultAsync();
+
+            return user.User;
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            var users = _context.Users.Where(
+                u => u.Orders.Any(o => o.Status == OrderStatus.Paid && o.CreatedAt.Year == 2010)
+            )
+            .ToListAsync();
+
+            return users;
+
+        }
+    }
+}
